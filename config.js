4c58709e52cb9ab6d68c5dc4396394349fb08e0a
Tanner <tannerphan@gmail.com>
2015-06-24 21:21:27 -0700

    Removed private data

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8080,
-	'database':'mongodb://tom:tomtom@proximus.modulusmongo.net:27017/aD2ohuqe',
-	'secret': 'facebookHackathon2015'
-};
---
+++ config_PERSONALIZE.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': //database link (previously used monglab)
+	'secret': //personal key used to hashfunction, part of authentication design currently
+};
